17fac5f89f1edf937f850f8b4be61e2dc1d72364
subas <subash.neruturi@gmail.com>
2022-10-13 10:54:05 +0530

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Crud_Operation_In_MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud_Operation_In_MVC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        Tutorials_CS _context = new Tutorials_CS();
+        public ActionResult Index()
+        {
+            var listofData = _context.Departments.ToList();
+
+            return View(listofData);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department model)
+        {
+            _context.Departments.Add(model);
+            _context.SaveChanges();
+            var listofData = _context.Departments.ToList();
+            ViewBag.Message = "Data Insert Successfully";
+            return View("Index", listofData);
+        }
+
+
+
+
+    }
+}
